2ed8f5a8d90c29f21dbde45f8129f34a22a7dd04
Althaaff <muhammadalthaf228@gmail.com>
2025-02-28 19:38:15 +0530

    Fix issues and add filtering by sortBy and category

--- client/src/pages/student/courses/index.jsx
+++ client/src/pages/student/courses/index.jsx
@@ -8,6 +8,7 @@ import {
   DropdownMenuRadioItem,
   DropdownMenu,
 } from "@/components/ui/dropdown-menu";
+import { Skeleton } from "@/components/ui/skeleton";
 import { filterOptions, sortOptions } from "@/config";
 import { StudentContext } from "@/context/student-context";
 import { fetchStudentViewCourseListService } from "@/services";
@@ -15,16 +16,25 @@ import { Label } from "@radix-ui/react-dropdown-menu";
 
 import { ArrowUpDownIcon } from "lucide-react";
 import { useContext, useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const StudentViewCoursesPage = () => {
   const [sort, setSort] = useState("price-lowtohigh");
   const [filters, setFilters] = useState({});
   const [searchParams, setSearchParams] = useSearchParams();
+
+  const navigate = useNavigate();
+
   // console.log("search params: ", searchParams.has("category"));
 
-  const { studentViewCoursesList, setStudentViewCoursesList } =
-    useContext(StudentContext);
+  const {
+    studentViewCoursesList,
+    setStudentViewCoursesList,
+    loadingState,
+    setLoadingState,
+  } = useContext(StudentContext);
+
+  console.log("loading state :", loadingState);
 
   const createSearchParamsHelper = (filterParams) => {
     const queryParams = [];
@@ -46,47 +56,23 @@ const StudentViewCoursesPage = () => {
   }, [filters]);
 
   useEffect(() => {
+    setSort("price-lowtohigh");
+    setFilters(JSON.parse(sessionStorage.getItem("filters")) || {});
+
+    console.log("filtered ");
+  }, []);
+
+  // remove the persisted filters from session storage when page loads :
+  useEffect(() => {
+    return () => {
+      sessionStorage.removeItem("filters");
+    };
+  }, []);
+
+  // testing :
+  useEffect(() => {
     console.log("Updated filters:", filters);
   }, [filters]); // Runs every time `filters` updates
-
-  // const handleFilterOnChange = (getSectionId, getCurrentOption) => {
-  //   console.log("console1", getSectionId, "console2", getCurrentOption);
-
-  //   let copyFilters = { ...filters };
-
-  //   const indexOfCurrentSection =
-  //     Object.keys(copyFilters).indexOf(getSectionId);
-
-  //   console.log(
-  //     "index :",
-  //     indexOfCurrentSection,
-  //     "getSectionId:",
-  //     getSectionId
-  //   );
-
-  //   // logic: if copy filter array empty --> [getSectionId]  dynamically sets the key in the object.  [getCurrentOption.id] --> creates an array with a single value.
-  //   if (indexOfCurrentSection === -1) {
-  //     copyFilters = {
-  //       ...copyFilters,
-  //       [getSectionId]: [getCurrentOption.id],
-  //     };
-
-  //     // console.log("filter :", copyFilters);
-  //   } else {
-  //     const indexOfCurrentOption = copyFilters[getSectionId].indexOf(
-  //       getCurrentOption.id
-  //     );
-
-  //     if (indexOfCurrentOption === -1) {
-  //       copyFilters[getSectionId].push(getCurrentOption.id); // adds
-  //     } else {
-  //       copyFilters[getSectionId].splice(indexOfCurrentOption, 1); // removes
-  //     }
-
-  //     setFilters(copyFilters);
-  //     sessionStorage.setItem("filters", JSON.stringify(copyFilters));
-  //   }
-  // };
 
   const handleFilterOnChange = (getSectionId, getCurrentOption) => {
     console.log("console1", getSectionId, "console2", getCurrentOption);
@@ -122,34 +108,44 @@ const StudentViewCoursesPage = () => {
     sessionStorage.setItem("filters", JSON.stringify(copyFilters));
   };
 
-  async function fetchAllStudentViewCourses() {
-    const response = await fetchStudentViewCourseListService();
+  async function fetchAllStudentViewCourses(filters, sort) {
+    console.log("sort :", sort);
+    console.log("filters :", filters);
+
+    const query = new URLSearchParams({
+      ...filters,
+      sortBy: sort,
+    });
+
+    const response = await fetchStudentViewCourseListService(query);
 
     // console.log("respons: ", response);
 
     if (response?.success) {
       setStudentViewCoursesList(response?.data);
+      setLoadingState(false);
     }
   }
 
   useEffect(() => {
-    fetchAllStudentViewCourses();
-  }, []);
+    if (filters !== null && sort !== null)
+      fetchAllStudentViewCourses(filters, sort);
+  }, [filters, sort]);
 
   return (
-    <div className="container p-4">
+    <div className="mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4">All Courses</h1>
 
       <div className="flex flex-col md:flex-row gap-4">
         <aside className="w-full md:w-64 space-y-4">
-          <div className="space-y-4">
+          <div>
             {/* filters */}
 
             {Object.keys(filterOptions).map((keyItem) => {
               // console.log(keyItem);
 
               return (
-                <div className="p-4 space-y-4" key={keyItem.id}>
+                <div className="p-4 " key={keyItem.id}>
                   <h3 className="font-bold mb-3 ">{keyItem.toUpperCase()}</h3>
 
                   <div className="grid gap-2 mt-2">
@@ -183,14 +179,14 @@ const StudentViewCoursesPage = () => {
           </div>
         </aside>
 
-        <div className="flex-1">
+        <div className="flex-1 w-full">
           <div className="flex justify-end items-center gap-2 mb-4">
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <Button
                   variant="outline"
                   size="sm"
-                  className="flex items-center gap-2 p-5"
+                  className="flex items-center gap-2 p-5 cursor-pointer"
                 >
                   <ArrowUpDownIcon className="h-4 w-4" />
                   <span className="text-[16px] font-medium">Sort By</span>
@@ -212,53 +208,67 @@ const StudentViewCoursesPage = () => {
                 </DropdownMenuRadioGroup>
               </DropdownMenuContent>
             </DropdownMenu>
-            <span className="text-sm text-black font-bold">10 Results</span>{" "}
+            <span className="text-sm text-black font-bold">
+              {studentViewCoursesList.length} Results
+            </span>{" "}
           </div>
 
           <div className="space-y-4">
             {studentViewCoursesList && studentViewCoursesList.length > 0 ? (
               studentViewCoursesList.map((courseItem) => (
-                <Card key={courseItem?._id} className="cursor-pointer">
-                  <CardContent className="flex gap-4 p-4">
-                    <div className="w-48 h-32 flex-shrink-0">
-                      <img
-                        src={courseItem?.image}
-                        alt="course image"
-                        className="w-full h-full object-cover"
-                      />
-                    </div>
-
-                    <div className="flex-1">
-                      <CardTitle className="text-xl mb-2">
-                        {courseItem?.title}
-                      </CardTitle>
-
-                      <p className="text-sm text-gray-500 font-bold mb-1">
-                        Created By {courseItem.instructorName}
-                      </p>
-
-                      <div className="flex items-center">
-                        <p className="text-[16px] text-gray-600 mb-2">{`${
-                          courseItem.curriculam.length
-                        } ${
-                          courseItem.curriculam.length <= 1
-                            ? "Lecture"
-                            : "Lectures"
-                        } - ${courseItem?.level.toUpperCase()} Lectures `}</p>
-
-                        <span className="text-[12px] text-gray-600 ml-2 mb-2 p-[3px] bg-white-300 border rounded-md font-extrabold hover:bg-white">
-                          {courseItem?.level}
-                        </span>
+                <>
+                  <Card
+                    key={courseItem?._id}
+                    className="cursor-pointer w-full"
+                    onClick={() =>
+                      navigate(`/course/details/${courseItem._id}`)
+                    }
+                  >
+                    <CardContent className="flex gap-4 p-4">
+                      <div className="w-48 h-32 flex-shrink-0">
+                        <img
+                          src={courseItem?.image}
+                          alt="course image"
+                          className="w-full h-full object-cover"
+                        />
                       </div>
-                      <p className="font-bold text-lg">
-                        ${courseItem?.pricing}
-                      </p>
-                    </div>
-                  </CardContent>
-                </Card>
+
+                      <div className="flex-1">
+                        <CardTitle className="text-xl mb-2">
+                          {courseItem?.title}
+                        </CardTitle>
+
+                        <p className="text-sm text-gray-500 font-bold mb-1">
+                          Created By {courseItem.instructorName}
+                        </p>
+
+                        <div className="flex items-center">
+                          <p className="text-[16px] text-gray-600 mb-2">{`${
+                            courseItem.curriculam.length
+                          } ${
+                            courseItem.curriculam.length <= 1
+                              ? "Lecture"
+                              : "Lectures"
+                          } - ${courseItem?.level.toUpperCase()} Lectures `}</p>
+
+                          <span className="text-[12px] text-gray-600 ml-2 mb-2 p-[3px] bg-white-300 border rounded-md font-extrabold hover:bg-white">
+                            {courseItem?.level}
+                          </span>
+                        </div>
+                        <p className="font-bold text-lg">
+                          ${courseItem?.pricing}
+                        </p>
+                      </div>
+                    </CardContent>
+                  </Card>
+                </>
               ))
+            ) : loadingState ? (
+              <Skeleton />
             ) : (
-              <h1>No Counses found!</h1>
+              <h1 className="font-extrabold text-2xl mr-2">
+                No Courses found!
+              </h1>
             )}
           </div>
         </div>
