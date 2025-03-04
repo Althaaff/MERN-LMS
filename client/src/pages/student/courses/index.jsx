0180f6d2a15cb4b86f781db2e166272191a96f33
Althaaff <muhammadalthaf228@gmail.com>
2025-03-04 14:12:54 +0530

    feat(student-view-courses): implement clear filters functionality & conditional navigation

--- client/src/pages/student/courses/index.jsx
+++ client/src/pages/student/courses/index.jsx
@@ -151,10 +151,18 @@ const StudentViewCoursesPage = () => {
     }
   }
 
+  // handle Clear filters :
+  const handleClearFilters = () => {
+    setFilters({});
+    setSearchParams(new URLSearchParams());
+    sessionStorage.removeItem("filters");
+
+    console.log("filters cleared!");
+  };
+
   return (
     <div className="mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4">All Courses</h1>
-
+      <h1 className="text-2xl font-bold mb-4">All Courses</h1>
       <div className="flex flex-col md:flex-row gap-4">
         <aside className="w-full md:w-64 space-y-4">
           <div>
@@ -200,33 +208,41 @@ const StudentViewCoursesPage = () => {
 
         <div className="flex-1 w-full">
           <div className="flex justify-end items-center gap-2 mb-4">
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button
-                  variant="outline"
-                  size="sm"
-                  className="flex items-center gap-2 p-5 cursor-pointer"
-                >
-                  <ArrowUpDownIcon className="h-4 w-4" />
-                  <span className="text-[16px] font-medium">Sort By</span>
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end" className="w-[180px]">
-                <DropdownMenuRadioGroup
-                  value={sort}
-                  onValueChange={(value) => setSort(value)}
-                >
-                  {sortOptions.map((sortItem) => (
-                    <DropdownMenuRadioItem
-                      value={sortItem.id}
-                      key={sortItem.id}
-                    >
-                      {sortItem.label}
-                    </DropdownMenuRadioItem>
-                  ))}
-                </DropdownMenuRadioGroup>
-              </DropdownMenuContent>
-            </DropdownMenu>
+            <div className="flex items-center gap-4">
+              <button
+                onClick={handleClearFilters}
+                className="p-2 bg-blue-400 border-none rounded-md text-white cursor-pointer"
+              >
+                Clear Filters
+              </button>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    variant="outline"
+                    size="sm"
+                    className="flex items-center gap-2 p-5 cursor-pointer"
+                  >
+                    <ArrowUpDownIcon className="h-4 w-4" />
+                    <span className="text-[16px] font-medium">Sort By</span>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="w-[180px]">
+                  <DropdownMenuRadioGroup
+                    value={sort}
+                    onValueChange={(value) => setSort(value)}
+                  >
+                    {sortOptions.map((sortItem) => (
+                      <DropdownMenuRadioItem
+                        value={sortItem.id}
+                        key={sortItem.id}
+                      >
+                        {sortItem.label}
+                      </DropdownMenuRadioItem>
+                    ))}
+                  </DropdownMenuRadioGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
             <span className="text-sm text-black font-bold">
               {studentViewCoursesList.length} Results
             </span>{" "}
