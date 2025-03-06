5dbd360da7d0ab1c97a3cec439529bda104969f6
Althaaff <muhammadalthaf228@gmail.com>
2025-03-06 18:33:31 +0530

    Add missing route file for fetching purchase info of the students

--- server/routes/student-routes/student.routes.js
+++ server/routes/student-routes/student.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  checkCoursePurchaseInfo,
   getAllStudentViewCourses,
   getStudentViewCourseDetails,
 } from "../../controllers/student-controller/course.controller.js";
@@ -7,6 +8,6 @@ import {
 const router = express.Router();
 
 router.get("/get", getAllStudentViewCourses);
-router.get("/get/details/:id", getStudentViewCourseDetails);
-
+router.get("/get/details/:id/:studentId", getStudentViewCourseDetails);
+router.get("/purchase-info/:id/:studentId", checkCoursePurchaseInfo);
 export default router;
