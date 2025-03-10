39fe4e3d792c758394e7974c5fb65f4e37b1ab20
Althaaff <muhammadalthaf228@gmail.com>
2025-03-10 18:29:35 +0530

    Add getAllCourseProgressPercentage Routes

--- server/routes/student-routes/course-progress.routes.js
+++ server/routes/student-routes/course-progress.routes.js
@@ -3,12 +3,18 @@ import {
   getCurrentCourseProgress,
   markCurrentLectureAsViewed,
   resetCurrentCourseProgress,
+  getAllCourseProgressPercentage,
 } from "../../controllers/student-controller/course.progress.controller.js";
+import { authenticateMiddleware } from "../../middleware/auth-middleware.js";
 
 const router = express.Router();
 
 router.get("/get/:userId/:courseId", getCurrentCourseProgress);
 router.post("/mark-lecture-viewed", markCurrentLectureAsViewed);
 router.post("/reset-progress", resetCurrentCourseProgress);
-
+router.get(
+  "/get/progress-percentage",
+  authenticateMiddleware,
+  getAllCourseProgressPercentage
+);
 export default router;
