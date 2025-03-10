8ecd31a667922f3d4dbbd1bb96cafb24ba31e8d2
Althaaff <muhammadalthaf228@gmail.com>
2025-03-10 18:24:43 +0530

    updated course-progress-model

--- server/models/course.progress.model.js
+++ server/models/course.progress.model.js
@@ -1,19 +1,19 @@
 import mongoose, { Schema } from "mongoose";
 
 const LectureProgressSchema = new Schema({
-  lectureId: String,
-  viewed: Boolean,
-  dateViewed: Date,
+  lectureId: { type: mongoose.Schema.Types.ObjectId, ref: "Lecture" }, // use ObjectId for reference
+  viewed: { type: Boolean, default: false },
+  dateViewed: { type: Date, default: null },
 });
 
 const CourseProgressSchema = new Schema({
-  userId: String,
-  courseId: String,
-  completed: Boolean,
-  completionDate: Date,
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // use ObjectId
+  courseId: { type: mongoose.Schema.Types.ObjectId, ref: "Course" }, // use ObjectId
+  completed: { type: Boolean, default: false },
+  completionDate: { type: Date, default: null },
   lecturesProgress: [LectureProgressSchema],
 });
 
-const CourseProgress = mongoose.model("Progress", CourseProgressSchema);
+const CourseProgress = mongoose.model("CourseProgress", CourseProgressSchema);
 
 export { CourseProgress };
