c1b99de76533aaae3dde8ebe48df503db98fc5f7
Althaaff <muhammadalthaf228@gmail.com>
2025-04-05 22:48:25 +0530

    Implement quiz model for managing quiz questions and answers

--- server/models/quizz.model.js
+++ server/models/quizz.model.js
@@ -1,3 +1,28 @@
 import mongoose, { Schema } from "mongoose";
 
-export const QuizzForm = new Schema({});
+export const questionSchema = new Schema({
+  question: String,
+  options: [String],
+  correctAnswerIndex: Number,
+});
+
+export const quizSchema = new Schema(
+  {
+    course: {
+      type: mongoose.Types.ObjectId,
+      ref: "Course",
+      required: true,
+    },
+    title: String,
+    questions: [questionSchema],
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const Quiz = mongoose.model("Quiz", quizSchema);
+
+export { Quiz };
